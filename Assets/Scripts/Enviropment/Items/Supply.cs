62188054275844c760e51ac196cd4ceb74b4b70a
Lucas <lucassousa180810@gmail.com>
2026-08-14 19:07:30 -0300

    feat: melhorado arquivo de suprimentos, não será necessária a sincronização manual das informações dele com o SupplyData

--- Assets/Scripts/Enviropment/Items/Supply.cs
+++ Assets/Scripts/Enviropment/Items/Supply.cs
@@ -5,35 +5,24 @@ public class Supply : MonoBehaviour
 {
     [SerializeField] GameObject prefab;
 
-    public float fillHungry = 0;
-    public float fillThirst = 0;
-    public float fillHealth = 0;
-    
-    public int quantityOfUses = 0;
-    public int maxUses = 1;
-    public Vector3 position;
-    public string itemName;
+    SupplyData data = new SupplyData();
+
+    int quantityOfUses = 0;
 
     public Supply(SupplyData supplyData)
     {
-        fillHealth = supplyData.fillHealth;
-        fillHungry = supplyData.fillHungry;
-        fillThirst = supplyData.fillThirst;
-
-        maxUses = supplyData.maxUses;
-        position = supplyData.position;
-        itemName = supplyData.itemName;
+        data = supplyData;
     }
 
     public void Use()
     {
-        Player.Instance.AddHealth(fillHealth);
-        Player.Instance.AddSatiety(fillHungry);
-        Player.Instance.AddHydratation(fillThirst);
+        Player.Instance.AddHealth(data.fillHealth);
+        Player.Instance.AddSatiety(data.fillHungry);
+        Player.Instance.AddHydratation(data.fillThirst);
 
         quantityOfUses++;
 
-        if(quantityOfUses >= maxUses)
+        if(quantityOfUses >= data.maxUses)
             Destroy(gameObject);
     }
 }
